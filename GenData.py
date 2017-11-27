7a7cd043737f847b8d7c5479c6cc2fb89acbdeb3
victore <victelk@gmail.com>
2017-11-27 12:12:40 +0200

    One hidden layer with dropout

--- GenData.py
+++ GenData.py
@@ -1,12 +1,12 @@
 import numpy as np
 import cv2
 import os.path
+import csv
 
 '''
 Generate gray image with random brightness pixels.
 Calculate average brightness of the pixels.
-Label each pixel with brigness bigger than the average as 255,
-and 0 otherwise.
+Label each image by its average brightness.
 '''
 
 
@@ -17,21 +17,28 @@ class GenData:
 
     # Generate num images:
     def generate(self, file_prefix, num):
+        dir_name = "data"
+        label_file_name = file_prefix + "_" + "labels" + ".csv"
+        f=open(os.path.join(dir_name, label_file_name),"w")
         width = self.width
         height = self.height
+        all_mean=np.array([])
         for k in range(num):
             img = np.random.randint(256, size=(width, height))
-            img_labels = np.zeros((width, height))
-            img_labels[img > img.mean()] = 255
-            dir_name = "data"
-            image_name = file_prefix + "_" + "image" + str(k) + ".png"
+            # img_labels = np.zeros((width, height))
+            # img_labels[img > img.mean()] = 255
+            image_name = file_prefix + "_" + "image" + str(k).zfill(3) + ".png"
             cv2.imwrite(os.path.join(dir_name, image_name), img)
-            image_labels_name = file_prefix + "_" + "label_image" + str(k) + ".png"
-            cv2.imwrite(os.path.join(dir_name, image_labels_name), img_labels)
+            f.write(str(img.mean()) + "\n")
+            all_mean=np.append(all_mean,img.mean())
+            # image_labels_name = file_prefix + "_" + "label_image" + str(k) + ".png"
+            # cv2.imwrite(os.path.join(dir_name, image_labels_name), img_labels)
+        f.close()
+        print(all_mean.mean())
 
 
 if __name__ == "__main__":
     gd = GenData(6, 6)
-    gd.generate("train", 100)
-    gd.generate("test", 50)
-    gd.generate("predict", 2)
+    gd.generate("train", 10000)
+    gd.generate("test", 1000)
+    gd.generate("predict", 10)
--- ToySegmentation.py
+++ ToySegmentation.py
@@ -4,27 +4,23 @@ import os.path
 import glob
 import tensorflow as tf
 
-LEARNING_RATE = 0.00001
+LEARNING_RATE = 0.000001
 tf.logging.set_verbosity(tf.logging.WARN)
 
 
-def my_input_fn(imagenames, label_imagenames, repeat_count=1):
-    def input_parser(img_path, label_img_path):
+def my_input_fn(imagenames, labels, repeat_count=1):
+    def input_parser(img_path, label):
         # read the img from file
         img_file = tf.read_file(img_path)
         img_decoded = tf.image.decode_image(img_file, channels=1)
         img_decoded.set_shape([6, 6, 1])
         img_flatten = tf.to_float(tf.reshape(img_decoded, [-1]))
-        label_img_file = tf.read_file(label_img_path)
-        label_img_decoded = tf.image.decode_image(label_img_file, channels=1)
-        label_img_decoded.set_shape([6, 6, 1])
-        label_img_flatten = tf.to_float(tf.reshape(label_img_decoded, [-1]))
-        return {'pixels': img_flatten}, label_img_flatten  # one_hot
+        return {'pixels': img_flatten}, label
 
-    dataset = (tf.data.Dataset.from_tensor_slices((imagenames, label_imagenames))
+    dataset = (tf.data.Dataset.from_tensor_slices((imagenames, labels.target))
                .map(input_parser))  # Transform each elem by applying input_parser fn
     dataset = dataset.repeat(repeat_count) #Epoch number
-    dataset = dataset.batch(10)  # Batch size to use
+    dataset = dataset.batch(1)  # Batch size to use
     iterator = dataset.make_one_shot_iterator()
     batch_features, batch_labels = iterator.get_next()
     return batch_features, batch_labels
@@ -36,21 +32,26 @@ def toy_segmentation_model_fn(features, labels, mode, params):
     # Connect the first hidden layer to input layer
     # (features["pixels"]) with relu activation
     first_hidden_layer = tf.layers.dense(features["pixels"], 36, activation=tf.nn.relu)
+    if mode == tf.estimator.ModeKeys.TRAIN:
+        first_hidden_layer = tf.layers.dropout(first_hidden_layer, rate=params["dropout"], training=True)
 
-    # Connect the second hidden layer to first hidden layer with relu
-    second_hidden_layer = tf.layers.dense(first_hidden_layer, 36, activation=tf.nn.relu)
+    # # Connect the second hidden layer to first hidden layer with relu
+    # second_hidden_layer = tf.layers.dense(first_hidden_layer, 36, activation=tf.nn.relu)
+    # if mode == tf.estimator.ModeKeys.TRAIN:
+    #     second_hidden_layer = tf.layers.dropout(second_hidden_layer, rate=params["dropout"], training=True)
 
     # Connect the output layer to second hidden layer (no activation fn)
-    output_layer = tf.layers.dense(second_hidden_layer, 36)
+    output_layer = tf.layers.dense(first_hidden_layer, 1)
 
     # Reshape output layer to 1-dim Tensor to return predictions
-    predicted = tf.reshape(output_layer, [-1,36])
+    predicted = tf.reshape(output_layer, [-1])
 
     # Provide an estimator spec for `ModeKeys.PREDICT`.
     if mode == tf.estimator.ModeKeys.PREDICT:
         return tf.estimator.EstimatorSpec(
             mode=mode,
-            predictions={"pixels": predicted})
+            predictions={"mean": predicted})
+
 
     # Calculate loss using mean squared error
     loss = tf.losses.mean_squared_error(labels, predicted)
@@ -77,33 +78,39 @@ def main(unused_argv):
 
     file_path = "./data"
     train_filenames = tf.convert_to_tensor(sorted([f for f in glob.glob(file_path + os.sep + "train_image*.png")]))
-    train_label_filenames = tf.convert_to_tensor(sorted([f for f in glob.glob(file_path + os.sep + "train_label_image*.png")]))
+    train_set = tf.contrib.learn.datasets.base.load_csv_without_header(
+        filename=file_path + os.sep + "train_labels.csv", target_dtype=np.float64, features_dtype=np.float64)
 
     test_filenames = tf.convert_to_tensor(sorted([f for f in glob.glob(file_path + os.sep + "test_image*.png")]))
-    test_label_filenames = tf.convert_to_tensor(sorted([f for f in glob.glob(file_path + os.sep + "test_label_image*.png")]))
+    test_set = tf.contrib.learn.datasets.base.load_csv_without_header(
+        filename=file_path + os.sep + "test_labels.csv", target_dtype=np.float64, features_dtype=np.float64)
 
     predict_filenames = tf.convert_to_tensor(sorted([f for f in glob.glob(file_path + os.sep + "predict_image*.png")]))
-    predict_label_filenames = tf.convert_to_tensor(sorted([f for f in glob.glob(file_path + os.sep + "predict_label_image*.png")]))
+    predict_set = tf.contrib.learn.datasets.base.load_csv_without_header(
+        filename=file_path + os.sep + "predict_labels.csv", target_dtype=np.float64, features_dtype=np.float64)
 
     # Set model params
-    model_params = {"learning_rate": LEARNING_RATE}
+    model_params = {"learning_rate": LEARNING_RATE,
+                    "dropout" : 0.2}
 
     # Instantiate Estimator
     nn = tf.estimator.Estimator(model_fn=toy_segmentation_model_fn, params=model_params, model_dir = "/tmp/tf_toy_segm")
 
     # Train
-    nn.train(input_fn=lambda: my_input_fn(train_filenames, train_label_filenames, repeat_count=100))
+    nn.train(input_fn=lambda: my_input_fn(train_filenames, train_set, repeat_count=100))
 
     # Score accuracy
-    ev = nn.evaluate(input_fn=lambda: my_input_fn(test_filenames, test_label_filenames))
+    ev = nn.evaluate(input_fn=lambda: my_input_fn(test_filenames, test_set))
     print("Loss: %s" % ev["loss"])
     print("Root Mean Squared Error: %s" % ev["rmse"])
 
     # Print out predictions
-    predictions = nn.predict(input_fn=lambda: my_input_fn(predict_filenames, predict_label_filenames))
+    predictions = nn.predict(input_fn=lambda: my_input_fn(predict_filenames, predict_set))
     for i, p in enumerate(predictions):
+        print(p)
+'''        
          print(p["pixels"].reshape(6,6))
-'''
+
     dir_name = "data"
     for i, p in enumerate(predictions):
         print(p["pixels"].reshape(6,6))
